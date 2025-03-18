ec175f12c4b74a2956dd602d801c5e6c5d515bbc
Piotr <pioxpl12@gmail.com>
2025-03-18 13:40:30 +0100

    Cape Swapper

---
+++ src/main/java/net/piox/CapeSwapper/CapeSwapper.java
@@ -0,0 +1,17 @@
+package net.piox.CapeSwapper;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CapeSwapper implements ModInitializer {
+	public static final String MOD_ID = "capeswapper";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/piox/CapeSwapper/CapeSwapperClient.java
@@ -0,0 +1,10 @@
+package net.piox.CapeSwapper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CapeSwapperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
